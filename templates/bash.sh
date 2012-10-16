78e28fcf836678dd1da791d6f385694df705fe8b
Curtis Sand <curtis.sand@emc.com>
2012-10-16 10:37:31 -0600

    Upgraded the bash.sh template to use getopt to parse options

--- templates/bash.sh
+++ templates/bash.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-# Print Usage
-function usage () {
-    echo "Usage info goes here"
+SCRIPT=$0
+
+usage () {
+    echo "$SCRIPT [-h|--help]";
+    exit 1;
 }
 
-# Script Scope Variables
-
-
-# Function Definitions
-function foo () {
-    echo ""
-}
-
-
-# CMD Line Parsing and the body of the script
-if [[ $# -eq 0 ]]; then
-    foo
-elif [[ $# -eq 1 ]]; then
-    if [[ $1 = "-h" ]]; then
-        usage
-    else
-        usage
-    fi
-else
-    usage
+# Options with no args have no colon
+# Option with required arg has 1 colon
+# Option with optional arg has 2 colons
+ARGS=`getopt -o "h" -l "help" -n "template.sh" -- "$@"`
+if [ $? -ne 0 ]; then
+    # bad arguments found
+    exit 1;
 fi
+eval set -- "$ARGS"
 
+while true ; do
+        case "$1" in
+                -h|--help) usage ; shift ;;
+                #-b|--b-long) echo "Option b, argument \`$2'" ; shift 2 ;;
+                #-c|--c-long)
+                #        # c has an optional argument. As we are in quoted mode,
+                #        # an empty parameter will be generated if its optional
+                #        # argument is not found.
+                #        case "$2" in
+                #                "") echo "Option c, no argument"; shift 2 ;;
+                #                *)  echo "Option c, argument \`$2'" ; shift 2 ;;
+                #        esac ;;
+                --) shift ; break ;;
+                *) echo "Internal error!" ; exit 1 ;;
+        esac
+done
