e929e21410798be3925ad6947a3bf80427d3abd1
Curtis Sand <curtis.sand@emc.com>
2014-06-19 10:34:49 -0600

    Playing with C++ virtual keyword and inheritence

--- cpp/test/test.cc
+++ cpp/test/test.cc
@@ -3,25 +3,21 @@
 #include "test.h"
 
 using namespace std;
+using namespace foobar;
 
-//void test_decoder(void){
-//    union decoder dcdr;
-//    dcdr.data = 0x12345678;
-//    printf("word.hi = 0x%x\n", dcdr.word.hi);
-//    printf("word.low = 0x%x\n", dcdr.word.low);
-//    printf("nb.hh = 0x%x\n", dcdr.nb.hh);
-//    printf("nb.hl = 0x%x\n", dcdr.nb.hl);
-//    printf("nb.lh = 0x%x\n", dcdr.nb.lh);
-//    printf("nb.ll = 0x%x\n", dcdr.nb.ll);
-//}
+Test::Test(int num)
+{ data=num; }
 
-Test::Test(int num) {
-    data=num;
-}
+int
+Test::get_val(void)
+{ return data++; }
 
-int Test::get_val(void){
-    return data++;
-}
+Foo::Foo(): Test(99)
+{};
+
+int
+Foo::get_val()
+{ return 999; }
 
 int main(int argc, char **argv){
     int num=0;
@@ -30,5 +26,7 @@ int main(int argc, char **argv){
     Test test_obj = Test(num);
     cout << "Value is " << test_obj.get_val() << endl;
     cout << "Value is now " << test_obj.get_val() << endl;
+    cout << "Banana is " << banana.get_val() << endl;
+
     return 0;
 };
--- cpp/test/test.h
+++ cpp/test/test.h
@@ -1,32 +1,20 @@
 #ifndef _TEST_H
 #define _TEST_H
 
-///* decoder is a quick way to decode 32 bits of data into 8
-// * bit chunks;
-// */
-//union decoder {
-//    uint32_t data; // set this
-//    struct {
-//        uint16_t low;
-//        uint16_t hi;
-//    } word;
-//    struct {
-//        uint8_t ll;
-//        uint8_t lh;
-//        uint8_t hl;
-//        uint8_t hh;
-//    } nb;
-//};
-//
-//void test_decoder(void);
+namespace foobar {
+    class Test {
+        public:
+            Test(int num);
+            virtual int get_val();
+        private:
+            int data;
+    };
 
-class Test {
-    public:
-        Test(int num);
+    struct Foo : public Test {
+        Foo();
         int get_val();
-    private:
-        int data;
-};
+    } banana;
 
+};
 
 #endif //_TEST_H
