a50ef75487ec4ae210746df54f043cd86c891a64
Curtis Sand <curtis.sand@emc.com>
2014-08-11 08:15:09 -0600

    Added dispatcher python library to parse cmdline args and call action methods

---
+++ py_lib/dispatcher.py
@@ -0,0 +1,81 @@
+import os, sys
+from argparse import ArgumentParser
+
+class Dispatcher:
+    def __init__(self, actions, default_action=None, common_action_args=None,
+                 add_non_action_arguments_cb=None, description=None):
+        # Actions => { ACTION_NAME: (ARGUMENTS_DICT, CALLABLE) }
+        # ARGUMENTS_DICT is kwargs for the ArgumentParser add_argument method.
+        # The special key 'short' is used to denote the short option string.
+        self.actions = actions
+
+        # If default_action is None then user must supply an action option
+        self.default_action = default_action
+
+        # description for ArgumentParser
+        self.description = description
+
+        self.common_action_args = {'dest': 'action', 'action': 'store_const'}
+        if common_action_args:
+            self.common_action_args.update(common_action_args)
+
+        # non-action arguments cb will be given a parser argument
+        self.add_non_action_arguments_cb=add_non_action_arguments_cb
+
+        self._args_parsed = False
+        self._parsed_args = None
+
+    def _add_action_argument_group(self, parser):
+        action_group = parser.add_argument_group('actions')
+        for opt, (kwargs, _) in self.actions.iteritems():
+            short = None
+            try:
+                short = kwargs.pop('short')
+            except KeyError:
+                pass
+            if 'const' not in kwargs.keys():
+                kwargs['const'] = opt
+            kwargs.update(self.common_action_args)
+            if short:
+                option = ('-' + short, '--' + opt)
+            else:
+                option = ('--' + opt,)
+            action_group.add_argument(*option, **kwargs)
+        return action_group
+
+    def parse_args(self):
+        parser = ArgumentParser(description=self.description)
+        self._add_action_argument_group(parser)
+        if self.add_non_action_arguments_cb:
+            self.add_non_action_arguments_cb(parser)
+        parser.add_argument(dest='args', nargs='*', type=str, metavar='ARG')
+        namespace, _ = parser.parse_known_args()
+        if namespace.action == None:
+            namespace.action = self.default_action
+        self._args_parsed = True
+        self._parsed_args = namespace
+        return self._parsed_args
+
+    def do_action(self, **kwargs):
+        if not self._args_parsed:
+            self.parse_args()
+        self.actions[self._parsed_args.action][1](self._parsed_args.args, **kwargs)
+
+if __name__=="__main__":
+
+    def example_action(args):
+        print "example_action: %s" % args
+
+    EXAMPLE_ACTIONS = {
+        'foobar': ({'short': 'f', 'help': 'foo bar baz.'}, example_action),
+    }
+
+    def example_add_non_action_arguments_cb(parser):
+        parser.add_argument('-t', '--test', action='store', type=str,
+                            default='testing', help="Random Test Option.")
+
+    example = Dispatcher(
+            EXAMPLE_ACTIONS, default_action='foobar',
+            add_non_action_arguments_cb=example_add_non_action_arguments_cb,
+            description='A Description of the program')
+    example.do_action()
