0106392f295390979b3018e2ddf117e76c5e2e2b
Curtis Sand <curtis.sand@emc.com>
2015-09-08 09:24:58 -0600

    templates: fixed indentation in bash.sh template

--- templates/bash.sh
+++ templates/bash.sh
@@ -18,18 +18,18 @@ fi
 eval set -- "$ARGS"
 
 while true ; do
-        case "$1" in
-                -h|--help) usage ; shift ;;
-                #-b|--b-long) echo "Option b, argument \`$2'" ; shift 2 ;;
-                #-c|--c-long)
-                #        # c has an optional argument. As we are in quoted mode,
-                #        # an empty parameter will be generated if its optional
-                #        # argument is not found.
-                #        case "$2" in
-                #                "") echo "Option c, no argument"; shift 2 ;;
-                #                *)  echo "Option c, argument \`$2'" ; shift 2 ;;
-                #        esac ;;
-                --) shift ; break ;;
-                *) echo "Internal error!" ; exit 1 ;;
-        esac
+    case "$1" in
+        -h|--help) usage ; shift ;;
+        #-b|--b-long) echo "Option b, argument \`$2'" ; shift 2 ;;
+        #-c|--c-long)
+        #    # c has an optional argument. As we are in quoted mode,
+        #    # an empty parameter will be generated if its optional
+        #    # argument is not found.
+        #    case "$2" in
+        #            "") echo "Option c, no argument"; shift 2 ;;
+        #            *)  echo "Option c, argument \`$2'" ; shift 2 ;;
+        #    esac ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
 done
