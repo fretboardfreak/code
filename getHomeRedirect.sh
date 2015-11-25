6b4122d66e292becf54bc42109519a60186cd8e4
Curtis Sand <curtis@curtissand.com>
2015-11-25 09:33:45 -0700

    getHomeRedirect.sh : use dig instead of a silly http service

--- getHomeRedirect.sh
+++ getHomeRedirect.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo -e "<DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n<html>\n<head>\n<title>Fret's Home</title>\n<meta http-equiv=\"REFRESH\" content=\"0;url=http://`curl ifconfig.me`\">\n</head>\n<body>`curl ifconfig.me`</body>\n</html>\n"
+echo -e "<DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n\
+<html>\n<head>\n<title>Fret's Home</title>\n<!--meta http-equiv=\"REFRESH\" \
+content=\"0;url=http://\
+$(dig +short myip.opendns.com @resolver1.opendns.com)\"-->\n</head>\n\
+<body>$(dig +short myip.opendns.com @resolver1.opendns.com)\
+</body>\n</html>\n"
