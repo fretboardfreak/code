b1cbd2e46bd80fe86e05c633d2a2ee5a0c11bf16
Curtis Sand <curtis.sand@emc.com>
2015-11-26 13:10:12 -0700

    profile_bash.sh: fleshed out the script for usability

--- profile_bash.sh
+++ profile_bash.sh
@@ -1,25 +1,81 @@
 #!/bin/bash
 
-# Profile Bash Scripts
-# This script is used to collate profiling information that would be written by
-# the following code.  Wrap this around the bash code you wish to profile...
-#
-# exec 3>&2 2> >(tee /tmp/profile.$$.log |\
-#                sed -u 's/^.*$/now/' |\
-#                date -f - +%s.%N > /tmp/profile.$$.time)
-# set -x
-# ... the bash code you want to profile
-# set +x
-# exec 2>&3 3>&-
-#
-# Note: this code was stolen from one of the answers to this stack overflow
-# topic:
-# http://stackoverflow.com/questions/5014823/how-to-profile-a-bash-shell-script
+SCRIPT=$0
+
+DIR="./"
+
+usage () {
+    echo "$SCRIPT [-h|--help] [OPTIONS] PID";
+    echo -e "\nProfile Bash Scripts\n--------------------"
+    echo "This script is used to collate profiling information that would be"
+    echo "written by the following bash code.  Wrap this around the bash code"
+    echo "you wish to profile..."
+    echo ""
+    echo "exec 3>&2 2> >(tee /tmp/profile.\$\$.log |\""
+    echo "               sed -u 's/^.*$/now/' |\""
+    echo "               date -f - +%s.%N > /tmp/profile.\$\$.time)"
+    echo "set -x"
+    echo "... the bash code you want to profile"
+    echo "set +x"
+    echo "exec 2>&3 3>&-"
+    echo ""
+    echo "  Options:"
+    echo "    -h|--help     Show this help message."
+    echo "    -d|--dir DIR  Directory containing the *.time and *.log files."
+    echo ""
+    echo "Note: this code was borrowed from one of the answers to this stack overflow"
+    echo "topic:"
+    echo "http://stackoverflow.com/questions/5014823/how-to-profile-a-bash-shell-script"
+
+    exit 1;
+}
+
+ARGS=`getopt -o "hd:" -l "help,dir:" -n "profile_bash.sh" -- "$@"`
+if [ $? -ne 0 ]; then
+    # bad arguments found
+    exit 1;
+fi
+eval set -- "$ARGS"
+
+while true ; do
+    case "$1" in
+        -h|--help) usage ; shift ;;
+        -d|--dir) DIR=$2 ; shift 2 ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
 
 PID=$1
 
-TIMES=/tmp/profile.${PID}.time
-LOG=/tmp/profile.${PID}.log
+case "$DIR" in  # ensure trailing slash in path
+    */) ;; # noop
+    *) $DIR="${DIR}/"
+esac
+
+base=${DIR}*.${PID}
+TIMES=$(readlink -f ${base}.time)
+LOG=$(readlink -f ${base}.log)
+RESULT=$(echo $LOG | sed 's/log$/results/g')
+base=  # unset temp var
+
+# check that TIMES and LOG files exist
+if [ ! -f ${TIMES} ] ; then
+    echo "Cannot find timestamp file at path '${TIMES}'"
+    usage
+    exit 1
+fi
+if [ ! -f ${LOG} ] ; then
+    echo "Cannot find log file at path '${LOG}'"
+    usage
+    exit 1
+fi
+
+# check that RESULT file does not exist
+if [ -f ${RESULT} ] ; then
+    echo "Result file already exists, '${RESULT}'. Cannot proceed."
+    exit 1;
+fi
 
 paste <(
     while read time ;do
@@ -29,5 +85,7 @@ paste <(
         printf "%12.9f %12.9f\n" ${crt:0:${#crt}-9}.${crt:${#crt}-9} \
                                  ${ctot:0:${#ctot}-9}.${ctot:${#ctot}-9}
         last=${time//.}
-      done < ${TIMES}
-  ) ${LOG} > /tmp/profile.${PID}.results
+    done < ${TIMES}
+) ${LOG} > ${RESULT}
+
+echo "Results have been written to '${RESULT}'."
